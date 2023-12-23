654f2b4347cdf20b67931f218182ab77873f69d4
giovannijunseokim <kingjunseo@gmail.com>
2023-12-23 14:34:14 +0900

    PureFirebaseMessagingService init

---
+++ app/src/main/java/org/sopt/dosopt/PureFirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package org.sopt.dosopt
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class PureFirebaseMessagingService {
+    // TODO : extend FirebaseMessagingService
+}
