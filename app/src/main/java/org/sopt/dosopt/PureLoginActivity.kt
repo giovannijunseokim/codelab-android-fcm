7a429f57241724e5403f2f7366a17419f04aa106
giovannijunseokim <kingjunseo@gmail.com>
2023-12-23 14:33:53 +0900

    PureLoginActivity init

---
+++ app/src/main/java/org/sopt/dosopt/PureLoginActivity.kt
@@ -0,0 +1,28 @@
+package org.sopt.dosopt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.dosopt.databinding.ActivityPureLoginBinding
+
+class PureLoginActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityPureLoginBinding.inflate(layoutInflater) }
+
+    // TODO : Declare the launcher at the top of your Activity/Fragment:
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            getFcmToken()
+        }
+    }
+
+    private fun getFcmToken() {
+        // TODO : Get FCM Token
+
+    }
+
+
+}
